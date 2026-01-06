4447fddca5ac646bf8d6c5ee9271ee68a1c577ad
François Margall <fr.margall@proton.me>
2026-01-06 18:28:16 +0100

    rewriting of createProgram function to make it more readable

--- src/gl.js
+++ src/gl.js
@@ -1,28 +1,31 @@
-function createProgram(gl, vsSrc, fsSrc) {
+/*
+ * creates WebGL program from vertex and fragment shader sources
+ */
+function createProgram(gl, vertexSource, fragmentSource) {
 
-    function compile(type, src) {
-        const s = gl.createShader(type);
-        gl.shaderSource(s, src);
-        gl.compileShader(s);
+    function compile(type, source) {
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
 
-        if (!gl.getShaderParameter(s, gl.COMPILE_STATUS)) {
-            console.error(src);
-            throw new Error(gl.getShaderInfoLog(s));
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error(source);
+            throw new Error(gl.getShaderInfoLog(shader));
         }
-        return s;
+        return shader;
     }
 
-    const vs = compile(gl.VERTEX_SHADER, vsSrc);
-    const fs = compile(gl.FRAGMENT_SHADER, fsSrc);
+    const vertexShader = compile(gl.VERTEX_SHADER, vertexSource);
+    const fragmentShader = compile(gl.FRAGMENT_SHADER, fragmentSource);
 
-    const p = gl.createProgram();
-    gl.attachShader(p, vs);
-    gl.attachShader(p, fs);
-    gl.linkProgram(p);
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
 
-    if (!gl.getProgramParameter(p, gl.LINK_STATUS)) {
-        throw new Error(gl.getProgramInfoLog(p));
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        throw new Error(gl.getProgramInfoLog(program));
     }
 
-    return p;
+    return program;
 }
